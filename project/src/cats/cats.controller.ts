01102df97f4a3102d1f362586140d044484a769f
bytrustu <bytrustu@gmail.com>
2022-07-14 01:46:30 +0900

    [exception] Exception Filter 처리

--- project/src/cats/cats.controller.ts
+++ project/src/cats/cats.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, UseFilters } from '@nestjs/common';
 import { CatsService } from './cats.service';
+import { HttpExceptionFilter } from '../http-exception.filter';
 
 @Controller('cats')
+@UseFilters(HttpExceptionFilter)
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
   @Get('/hello')
   getHello(): string {
     return this.catsService.hello();
+  }
+
+  @Get('/error')
+  getError() {
+    throw new HttpException('api is broken', HttpStatus.BAD_REQUEST);
+    return 'get call api'
   }
 }
