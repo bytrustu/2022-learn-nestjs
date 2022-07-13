687bf03e6734821720986e35996d43e82a063cf5
bytrustu <bytrustu@gmail.com>
2022-07-14 01:51:42 +0900

    [pipe] feat: pipe를 통한 param id 값 number validation

--- project/src/cats/cats.controller.ts
+++ project/src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, HttpException, HttpStatus, UseFilters } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Param, ParseIntPipe, UseFilters } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { HttpExceptionFilter } from '../http-exception.filter';
 
@@ -16,5 +16,11 @@ export class CatsController {
   getError() {
     throw new HttpException('api is broken', HttpStatus.BAD_REQUEST);
     return 'get call api'
+  }
+
+  @Get(':id')
+  getOneCat(@Param('id', ParseIntPipe) id) {
+    console.log(typeof id);
+    return `get call api ${id}`;
   }
 }
