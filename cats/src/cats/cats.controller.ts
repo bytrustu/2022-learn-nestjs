47cc2a6d3354f051b04968313982d68afa8cd531
bytrustu <bytrustu@gmail.com>
2022-07-17 00:46:18 +0900

    [cats] feat: controller 추가

--- cats/src/cats/cats.controller.ts
+++ cats/src/cats/cats.controller.ts
@@ -4,7 +4,7 @@ import {
   HttpException,
   HttpStatus,
   Param,
-  ParseIntPipe,
+  ParseIntPipe, Post,
   UseFilters,
   UseInterceptors,
 } from '@nestjs/common';
@@ -18,4 +18,29 @@ import { SuccessInterceptor } from '../common/interceptors/success.interceptor';
 export class CatsController {
   constructor(private readonly catsService: CatsService) {}
 
+
+  @Get()
+  getCurrentCat() {
+    return 'current cat';
+  }
+
+  @Post()
+  async signUp() {
+    return 'signup';
+  }
+
+  @Post('login')
+  logIn() {
+    return 'login';
+  }
+
+  @Post('logout')
+  logOut() {
+    return 'logout';
+  }
+
+  @Post('upload/cats')
+  uploadCatImg() {
+    return 'uploadImg';
+  }
 }
